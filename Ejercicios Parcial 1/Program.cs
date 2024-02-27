327b3a3f3767212e5179a8ab0c74e051bb0b1528
RudyGT05 <rgonzaleza11@miumg.edu.gt>
2024-02-27 16:14:41 -0600

    Ejercicio 4

--- Ejercicios Parcial 1/Program.cs
+++ Ejercicios Parcial 1/Program.cs
@@ -1,18 +1,27 @@
 ﻿using System.Timers;
 
-int num, tabla;
-
-Console.WriteLine("Ingrese el numero  que desea y se mostrará la su respectiva tabla de multiplicar del 1 al 10");
-num = int.Parse(Console.ReadLine());
-
-for (int i = 0; i <= 10; i++)
+int numsecreto;
+do
 {
-    tabla = num * i;
-    Console.WriteLine("*"+i+" = "+tabla);
+    Console.WriteLine("Ingresa numeros hasta que encuentres el numero secreto: ");
+    numsecreto = int.Parse(Console.ReadLine());
+    switch (numsecreto)
+    {
+        case >= 0 and <= 42:
+            Console.WriteLine("El numero secreto es mayor a este");
+            break;
+        case 43:
+            break;
+        case >= 44 and <= 100:
+            Console.WriteLine("El numero secreto es menor a este");
+            break;
+        default: Console.WriteLine(" Solo se permiten numeros del 1 al 100");
+            break;
+    }
 
-}
-
-
+} while (numsecreto != 43);
+Console.WriteLine("¡El numero secreto es 43! ¡Buen trabajo!");
+Console.WriteLine("Se cerrará el programa...");
 
     
 
